0c6772f5ebb1d344a09fe02a053cdeb22ab21c37
glorie <70677768+glorie-git@users.noreply.github.com>
2024-04-21 17:18:56 +0000

    Add 5 x 5 tabletop with robot and target (all hardcoded)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
+function Square({ value, id }) {
+  return (
+    <div className="square">
+      <div className="container" id={id}>
+        {value}
+      </div>
+    </div>
+  );
+}
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="tabletop">
+        <div className="grid-row">
+          <Square value="" id="0" />
+          <Square value="" id="1" />
+          <Square value="" id="2" />
+          <Square value="" id="3" />
+          <Square value="" id="4" />
+        </div>
+        <div className="grid-row">
+          <Square value="" id="5" />
+          <Square value="" id="6" />
+          <Square value="" id="7" />
+          <Square value="" id="8" />
+          <Square value="" id="9" />
+        </div>
+        <div className="grid-row">
+          <Square value="" id="10" />
+          <Square value="" id="11" />
+          <Square value="R" id="12" />
+          <Square value="" id="13" />
+          <Square value="" id="14" />
+        </div>
+        <div className="grid-row">
+          <Square value="" id="15" />
+          <Square value="" id="16" />
+          <Square value="" id="17" />
+          <Square value="" id="18" />
+          <Square value="T" id="19" />
+        </div>
+        <div className="grid-row">
+          <Square value="" id="20" />
+          <Square value="" id="21" />
+          <Square value="" id="22" />
+          <Square value="" id="23" />
+          <Square value="" id="24" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="controls"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
