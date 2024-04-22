05178cbff56c9528673262758782b3847384dd9f
glorie <70677768+glorie-git@users.noreply.github.com>
2024-04-22 16:12:43 +0000

    Removes create table function out of App function. Fixes bug of robot cell being overwritten with an empty string when target is found.

--- src/App.jsx
+++ src/App.jsx
@@ -11,33 +11,67 @@ function Square({ value, id }) {
   );
 }
 
-function App() {
-  const startingLocation = 12;
-  const [rotate, setRotate] = useState(0);
-  const [location, setLocation] = useState(startingLocation);
+function generateTargetLocation(location) {
+  const newTargetLocation = Math.floor(Math.random() * 25);
+  if (newTargetLocation !== location) {
+    return newTargetLocation;
+  } else {
+    return generateTargetLocation(location);
+  }
+}
 
-  function getTargetLocation() {
-    const newTargetLocation = Math.floor(Math.random() * 25);
-    // setTargetLocation(newTargetLocation);
+let targetLocation = null;
 
-    if (newTargetLocation !== location) {
-      return newTargetLocation;
-    } else {
-      return getTargetLocation();
-    }
+// Function to create tabletop using a double for loop
+// Paramter: rows and cols
+// Retun: A 2D array
+function createTabletop(rows, cols, roboLocation) {
+  let row = [];
+  let rowIndex = 0;
+
+  if (targetLocation === null) {
+    targetLocation = generateTargetLocation(roboLocation);
   }
 
-  const [targetLocation, setTargetLocation] = useState(getTargetLocation());
-  const [points, setPoints] = useState(0);
+  for (let r = 0; r < rows; r++) {
+    let col = [];
+    // create squares
+    for (let c = 0; c < cols; c++) {
+      const index = rows * r + c;
+      let value;
+
+      if (index === roboLocation) {
+        value = "R";
+      } else if (index === targetLocation) {
+        value = "T";
+      }
+
+      col.push(<Square key={index} value={value} id={index} />);
+    }
+    row.push(
+      <div key={rowIndex} className="grid-row">
+        {col}
+      </div>,
+    );
+    rowIndex++;
+  }
+  return row;
+}
+
+function App() {
+  const [rotate, setRotate] = useState(0);
+  const startingLocation = 12;
+  // TODO: Does not need to be a state variable
+  const [location, setLocation] = useState(startingLocation);
 
   useEffect(() => {
     if (location === targetLocation) {
       console.log("Target found.");
-      // increment points
-      setPoints(points + 1);
-
       // generate new target location
-      // const
+      const newTargetLocation = generateTargetLocation(location);
+
+      // // setTargetLocation(newTargetLocation);
+      targetLocation = newTargetLocation;
     }
   }, [location]);
 
@@ -56,14 +90,13 @@ function App() {
 
       setRotate(rotation);
     } else if (value === "forward") {
-      // Move forward
-
       let newLocation;
+
       if (rotate === 0) {
         newLocation = location - 5;
-      } else if (rotate === 90) {
+      } else if (rotate === 90 || rotate === -270) {
         newLocation = location + 1;
-      } else if (rotate === -90) {
+      } else if (rotate === -90 || rotate === 270) {
         newLocation = location - 1;
       } else if (rotate === 180 || rotate === -180) {
         newLocation = location + 5;
@@ -71,55 +104,18 @@ function App() {
 
       element.innerText = "";
       element.style.transform = "rotate(0deg)";
-      // console.log("New location: " + newLocation);
       const newElement = document.getElementById(newLocation);
-      // console.log(newElement);
       newElement.innerText = "R";
       newElement.style.transform = `rotate(${rotate}deg)`;
-
-      // console.log("New location: " + newLocation);
 
       setLocation(newLocation);
     }
   }
 
-  // Function to create tabletop using a double for loop
-  // Paramter: rows and cols
-  // Retun: A 2D array
-  function createTabletop(rows, cols) {
-    let row = [];
-    let rowIndex = 0;
-    for (let r = 0; r < rows; r++) {
-      let col = [];
-      // create squares
-      for (let c = 0; c < cols; c++) {
-        const index = rows * r + c;
-        let value;
-
-        if (index === 12) {
-          value = "R";
-        } else if (index === targetLocation) {
-          value = "T";
-        }
-
-        col.push(<Square key={index} value={value} id={index} />);
-      }
-      row.push(
-        <div key={rowIndex} className="grid-row">
-          {col}
-        </div>,
-      );
-      rowIndex++;
-    }
-
-    return row;
-  }
-
   return (
     <>
-      <div>Score: {points}</div>
       <div className="tabletop">
-        <div>{createTabletop(5, 5)}</div>
+        <div>{createTabletop(5, 5, location)}</div>
       </div>
       <div className="controls">
         <button onClick={() => handleClick("left")}>Rotate Left</button>
